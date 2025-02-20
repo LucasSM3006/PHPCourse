1108fbb2f95bdb23261d9601ce5e0eef5b4f426f
LucasSM3006 <sasso3006@gmail.com>
2025-02-19 23:13:49 -0300

    multiple string functions, most commonly used ones & link to the documentation w/ more

--- php-sandbox/01-variables-data-types/08-string-functions/index.php
+++ php-sandbox/01-variables-data-types/08-string-functions/index.php
@@ -1,5 +1,50 @@
 <?php
 $output = null;
+$string = "long string, yep";
+
+// strlen. length of string.
+
+$length = strlen($string);
+
+// str_word_count. counts how many words.
+
+$howManyWords = str_word_count($string);
+
+// strpos. position where the word starts, based on index.
+
+$indexOfWordString = strpos($string, "string");
+
+// get char by index
+
+$indexOfCharacter = $string[2];
+
+// substr. makes a substring, takes two parameters, start and end.
+
+$substring = substr($string, 5, 10);
+
+// str_replace. replaces substrings with something else.
+
+$replacedSubstring = str_replace("long", "VERY LONG", $string); // word we want to replace, word to replace, string to replace.
+
+// strtolower
+
+$lowerCaseString = strtolower($string);
+
+// strtoupper
+
+$upperCaseString = strtoupper($string);
+
+// ucwords. makes the first letter of each word uppercase
+
+$firstLetterUppercase = ucwords($string);
+
+// trim. trims empty space @ the start and finish.
+
+$trimmedString = trim("     empty space        ");
+
+// most commonly used functions, but there's more.
+// documentation: https://www.php.net/manual/en/ref.strings.php
+
 ?>
 
 <!DOCTYPE html>
@@ -20,7 +65,16 @@ $output = null;
   </header>
   <div class="container mx-auto p-4 mt-4">
     <div class="bg-white rounded-lg shadow-md p-6 mt-6">
-      <p class="text-xl"><?= $output ?></p>
+      <p class="text-xl">
+        <?= "Length of '" . $string . "': " . $length . "<br />" ?>
+        <?= "Word count of '" . $string . "': " . $howManyWords . "<br />" ?>
+        <?= "Index of when 'string' starts on '" . $string . "': " . $indexOfWordString . "<br />" ?>
+        <?= "Length of" . $string . ": " . $length . "<br />" ?>
+        <?= "Character at index 2 on '$string': $indexOfCharacter <br />" ?>
+        <?= "Substring of '$string' starting at index 5 and ending at 10: $substring <br />" ?>
+        <?= "Substring of '$string' with replacement of 'long': $replacedSubstring <br />" ?>
+
+    </p>
     </div>
   </div>
 </body>
