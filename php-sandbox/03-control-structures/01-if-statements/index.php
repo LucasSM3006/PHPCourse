a630ddf5b23b7646ddf44dadf5f6204a11b90374
LucasSM3006 <sasso3006@gmail.com>
2025-02-20 20:40:08 -0300

    Ifs and elses

--- php-sandbox/03-control-structures/01-if-statements/index.php
+++ php-sandbox/03-control-structures/01-if-statements/index.php
@@ -1,10 +1,52 @@
 <?php
-
+$age = 40;
+$name = "john bush";
 // If statement
-
+if($age >= 21)
+{
+echo "can drink";
+};
 // If-Else
+
+if($age >= 21)
+{
+echo "can drink";
+}
+else
+{
+echo "cannot drink";
+}
 
 // Nested if statement
 
+if($age >= 21)
+{
+    if($name == "john bush")
+    {
+        echo "hello bush";
+    }
+echo "can drink";
+}
+else if ($age >= 18)
+{
+echo "can drink at least";
+}
+else
+{
+    echo "youre a minor";
+}
+
 // If-Else-If
 
+if($age > 20)
+{
+
+}
+else if ($age >= 18)
+{
+echo "can drink at least";
+}
+else
+{
+    echo "youre a minor";
+}
