3f13106732346104a544edee9ac974f5fe3444e1
LucasSM3006 <sasso3006@gmail.com>
2025-02-20 13:36:53 -0300

    Multi-dimensional arrays, arrays holding arrays. Doesn't look too dissimilar to a JSON object to be honest.

--- php-sandbox/02-arrays-and-iteration/04-multi-dimensional-arrays/index.php
+++ php-sandbox/02-arrays-and-iteration/04-multi-dimensional-arrays/index.php
@@ -1,5 +1,31 @@
 <?php
 $output = null;
+
+$fruits = [
+  ['Apple', 'Red'],
+  ['Orange', 'Orange'],
+  ['Pear', 'Green']
+];
+
+$apple = $fruits[0][0];
+$orange = $fruits[1][0];
+$pear = $fruits[2][0];
+
+$appleColor = $fruits[0][1];
+$orangeColor = $fruits[1][1];
+$pearColor = $fruits[2][1];
+
+
+
+$users = [
+  ['name' => 'John', 'email' => 'johndoe@hmail.com', 'password' => '123456'],
+  ['name' => 'Marianne', 'email' => 'marianne@hmail.com', 'password' => '123456'],
+  ['name' => 'Rose', 'email' => 'rose@hmail.com', 'password' => '123456'],
+];
+
+$users[] = ['Joseph' => 'Rose', 'email' => 'joseph@hmail.com', 'password' => '123456'];
+
+$mariannesEmail = $users[1]['email'];
 ?>
 
 <!DOCTYPE html>
@@ -22,6 +48,19 @@ $output = null;
     <div class="bg-white rounded-lg shadow-md p-6 mt-6">
       <!-- Output -->
       <p class="text-xl"><?= $output ?></p>
+      <p class="text-xl"><?= $apple ?></p>
+      <p class="text-xl"><?= $orange ?></p>
+      <p class="text-xl"><?= $pear ?></p>
+      <p class="text-xl"><?= $appleColor ?></p>
+      <p class="text-xl"><?= $orangeColor ?></p>
+      <p class="text-xl"><?= $pearColor ?></p>
+      <br />
+      <p class="text-xl">
+        <pre>
+          <?php print_r($users);?>
+        </pre>
+      </p>
+      <p class = "text-xl"><?= $mariannesEmail ?></p>
     </div>
   </div>
 </body>
