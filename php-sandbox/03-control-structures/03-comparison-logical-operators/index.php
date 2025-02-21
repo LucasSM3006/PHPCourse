f4f85a2b755e50f4e6e4ff8cd3c247c54607808a
LucasSM3006 <sasso3006@gmail.com>
2025-02-20 21:08:07 -0300

    All comparers and logic operations

--- php-sandbox/03-control-structures/03-comparison-logical-operators/index.php
+++ php-sandbox/03-control-structures/03-comparison-logical-operators/index.php
@@ -1,5 +1,10 @@
 <?php
 
+function breakLine()
+{
+    echo "<br />";
+}
+
 /*
 | Comparison Operators
 | Operator | Description              |
@@ -15,6 +20,48 @@
 | >=       | Greater than or equal to |
 */
 
+$x = 10;
+$y = 10;
+
+var_dump($x == $y);
+breakLine();
+
+// now here's something that is both nifty but also kinda annoyin'.
+$a = 10;
+$b = '10';
+
+var_dump($a == $b); // This returns true. We need to do it like javascript for types.
+breakLine();
+var_dump($a === $b); // Both value and type, thus, returns false.
+breakLine();
+
+$differentToA = 11;
+$sameAsB = 10;
+
+// Not equal.
+
+var_dump($a != $differentToA); // true
+breakLine();
+var_dump($b != $sameAsB); // false
+breakLine();
+var_dump($a != $b); // false 'cause they hold the same value.
+breakline();
+var_dump($a !== $b); // true 'cause theyre not identical.
+breakline();
+var_dump($a <> $b); // false, afaik, same as !=
+breakLine();
+
+// Lesser/Equal/Greater
+
+var_dump($a > $b); // false, 10 == 10
+breakLine();
+var_dump($a < $b); // false, 10 == 10
+breakLine();
+var_dump($a >= $b); // true 'cause 10 is equal to 10
+breakLine();
+var_dump($a <= $b); // true 'cause 10 is equal to 10
+breakLine();
+
 
 /*
 | Logical Operators
@@ -27,3 +74,19 @@
 | xor      | True if one is true    |
 | !        | True if it is not true |
 */
+
+$a = 10;
+$b = 20;
+
+var_dump(10 == 10 && $b == 20);
+breakLine();
+var_dump(10 == 10 and $b == 20);
+breakLine();
+var_dump(10 == 1 || $b == 20);
+breakLine();
+var_dump(10 == 1 or $b == 20);
+breakLine();
+var_dump(10 == 10 xor $b == 20); // False. If more than one is true, it is false. Very... Rare.
+breakLine();
+var_dump(!(10 == 1)); // True because 10 is not equal to 1, which is false, which in turn, because of '!' is true.
+breakLine();
