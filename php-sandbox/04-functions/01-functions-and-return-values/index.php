dc51400123957bb341723c638447c514087b4d29
LucasSM3006 <sasso3006@gmail.com>
2025-02-21 18:58:40 -0300

    Functions, declaring, return.

---
+++ php-sandbox/04-functions/01-functions-and-return-values/index.php
@@ -0,0 +1,18 @@
+<?php
+
+function functionName()
+{
+    echo "Functions are kinda cool.";
+}
+
+function returnSomething()
+{
+    return "Something";
+}
+
+functionName();
+
+echo "<br />";
+
+$string = returnSomething();
+echo $string;
