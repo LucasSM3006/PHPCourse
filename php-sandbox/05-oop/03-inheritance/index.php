91dd4a10f762c58f9535195edb2a2c97d4f5a8d8
LucasSM3006 <sasso3006@gmail.com>
2025-02-22 18:53:47 -0300

    Inheritance, polymorphism, protected.

--- php-sandbox/05-oop/03-inheritance/index.php
+++ php-sandbox/05-oop/03-inheritance/index.php
@@ -17,3 +17,37 @@ class User
     echo $this->name . ' logged in <br>';
   }
 }
+
+class Admin extends User 
+{
+  public $level;
+
+  public function __construct($name, $email, $level)
+  {
+    $this->level = $level;
+    parent::__construct($name, $email); // Calls parent's constructor
+  }
+
+  public function login()
+  {
+    echo "Admin $this->name logged in <br>";
+  }
+
+  public function getStatus()
+  {
+    return $this->status;
+  }
+}
+
+$adm = new Admin("Mr. Powerful", "admwebsite@email.com", 5);
+
+echo '<br>';
+echo 'Name: ' . $adm->name . '<br>';
+echo 'Email: ' . $adm->email . '<br>';
+echo 'Level of permission ( 1 -> lowest, 5 -> highest)' . $adm->level . '<br>';
+
+$adm->login();
+
+echo "Status: " . $adm->getStatus() . "<br>";
+
+var_dump($adm);
