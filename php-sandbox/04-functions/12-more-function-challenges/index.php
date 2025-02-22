edb3a4c01d313df7a1b5742b80b079d2ab1445d5
LucasSM3006 <sasso3006@gmail.com>
2025-02-21 21:59:23 -0300

    More function challenges, F to C conversion, find the longest word in a sentence, print names in uppercase

--- php-sandbox/04-functions/12-more-function-challenges/index.php
+++ php-sandbox/04-functions/12-more-function-challenges/index.php
@@ -6,14 +6,50 @@
   The formula to convert Fahrenheit to Celsius is: Celsius = (Fahrenheit - 32) * 5/9
 */
 
+function fahrenheitToCelsius($temp)
+{
+  return ($temp - 39) * 5/9;
+}
+
+echo "90 F to C: " . fahrenheitToCelsius(90);
+echo '<br>';
+echo "120 F to C: " . fahrenheitToCelsius(120);
+echo '<br>';
+echo "150 F to C: " . fahrenheitToCelsius(150);
+echo '<br>';
+echo "32 F to C: " . fahrenheitToCelsius(32);
+echo '<br>';
 echo '<br>';
 
 /*
   Challenge 2: Print names in uppercase
-  Create a function called `printNamesToUpperCase` that takes an array of names as an argument. The function should loop through the array and print each name to the screen in uppercase letters.
+  Create a function called `printNamesToUpperCase` that takes an array of names as an argument.
+  The function should loop through the array and print each name to the screen in uppercase letters.
 */
 
+$names =
+[
+  "John",
+  "Maria",
+  "Juarez",
+  "Aiko",
+  "Yuki",
+  "Ichiban",
+  "Falkore"
+];
 
+function printNamesToUpperCase($names) 
+{
+  foreach($names as $name)
+  {
+    echo strtoupper($name);
+    echo '<br>';
+  }
+}
+
+echo printNamesToUpperCase($names);
+
+echo '<br>';
 echo '<br>';
 
 /*
@@ -22,3 +58,24 @@ echo '<br>';
   2. The function should return the longest word in the sentence.
   3. The output should look like this:
 */
+
+$longSentence = "The biggest possible word that exists in the world is REALLY big, but I have no idea what it could possibly be, so here's a long one in Portuguese: Otorrinolaringologista.";
+
+function findLongestWord($sentence)
+{
+  $longestWord = "";
+
+  $sentenceArray = explode(" ", $sentence);
+
+  foreach($sentenceArray as $word)
+  {
+    if(strlen($word) > strlen($longestWord))
+    {
+      $longestWord = $word;
+    }
+  }
+
+  return $longestWord;
+}
+
+echo findLongestWord($longSentence);
