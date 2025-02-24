d2ae45930385733c617dbba4957e9de70f3696ab
LucasSM3006 <sasso3006@gmail.com>
2025-02-24 17:29:28 -0300

    superglobal, used to get values from queries. Aka, ?value=10&name=example. Values passed on URL.

--- php-sandbox/06-superglobals/03-get/index.php
+++ php-sandbox/06-superglobals/03-get/index.php
@@ -1,3 +1,21 @@
 <?php
 // Add a query parameter to the URL in the browser's address bar:
 // http://localhost:8000/?name=John
+
+echo isset($_GET['name']) ? $_GET['name'] : "";
+
+echo "<br/>";
+
+echo $_GET['name'] ?? "";
+
+echo "<br/>";
+
+// the user can send anything through the queries, including javascript
+// so if we're displaying things on screen with an echo, they could run a <script></script> tag.
+// to not let it execute, we can do:
+
+echo htmlspecialchars($_GET['name'] ?? '');
+
+echo "<br/>";
+
+var_dump($_GET); // gets everything sent as a query through the website
