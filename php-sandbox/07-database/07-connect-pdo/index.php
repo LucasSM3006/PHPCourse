b791930b71f5a7e9425791ad98515e3ea6f5bf46
LucasSM3006 <sasso3006@gmail.com>
2025-02-25 19:56:41 -0300

    Database connection
    Database connection, necessary variables, and a try/catch block in case something goes wrong. Aka wrong passwords, user, mysql isn't running, etc.

--- php-sandbox/07-database/07-connect-pdo/index.php
+++ php-sandbox/07-database/07-connect-pdo/index.php
@@ -1,2 +1,27 @@
 <?php
 
+// Database credentials.
+$HOST = 'localhost';
+$PORT = 3306;
+$DB_NAME = 'blog';
+
+// User creds.
+$USERNAME = 'lucas_php';
+$PASSWORD = 'PHPPassword_hardDifficulty';
+
+$DSN = "mysql:host={$HOST};port={$PORT};dbname={$DB_NAME};charset=utf8";
+
+try
+{
+    // Create a PDO instance
+    $PDO = new PDO($DSN, $USERNAME, $PASSWORD);
+
+    // Set PDO to throw exceptions on error
+    $PDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    echo "Database connected...";
+}
+catch (PDOException $e)
+{
+    echo "Connection failed. Error message: " . $e->getMessage();
+}
