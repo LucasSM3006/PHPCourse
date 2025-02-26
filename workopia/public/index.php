8eb543250af5deaa6434dfddc402cd93a9a96627
LucasSM3006 <sasso3006@gmail.com>
2025-02-26 18:52:21 -0300

    Usage of helper function
    Uses 'basePath' to get the base path. It's a helper function inside helpers.php.

--- workopia/public/index.php
+++ workopia/public/index.php
@@ -1,6 +1,8 @@
 <?php
 
-require '../views/home.view.php';
+require '../helpers.php';
+
+require basePath('views/home.view.php');
 // require './views/home.view.php'; // also works fine
 
 echo "First echo";
