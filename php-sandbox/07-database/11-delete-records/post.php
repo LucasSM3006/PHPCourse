b717bc3caf1fbed218dfe8254c56f3c80464edbb
LucasSM3006 <sasso3006@gmail.com>
2025-02-25 21:56:23 -0300

    Deletion button & hidden inputs

--- php-sandbox/07-database/11-delete-records/post.php
+++ php-sandbox/07-database/11-delete-records/post.php
@@ -52,6 +52,13 @@ $post = $stmt->fetch();
         </div>
       </div>
     </div>
+    <form action="delete.php" method="POST">
+      <input type="hidden" name="_method" value="delete">
+      <input type="hidden" name="id" value="<?= $post['id'] ?>">
+      <button type="submit" name="submit" class="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 focus:outline-none">
+        Delete
+      </button>
+    </form>
   </div>
 </body>
 
