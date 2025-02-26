8770174261f29bd5e03c018734bcf443485a7423
LucasSM3006 <sasso3006@gmail.com>
2025-02-26 19:40:29 -0300

    LoadView and LoadPartial
    Loadview takes a name which is the name of the file and loads it from the views. Allowing us to pass only the first name of the file and not the extension. LoadPartial does the same thing but for partials.

--- workopia/helpers.php
+++ workopia/helpers.php
@@ -12,3 +12,34 @@ function basePath($path = '')
 {
     return __DIR__ . '/' . $path;
 }
+
+/**
+ * Loads a view by using a require.
+ * 
+ * @param string name
+ * @return void
+ * 
+ * Suffix of views is .view.php therefore it is done in a way where
+ * you can simply pass the name of the resource. Ie. $name = 'search' instead of $name = 'search.view.php'
+ */
+
+function loadView($name)
+{
+    require basePath('views/' . $name . '.view.php');
+}
+
+/**
+ * Loads a partial by using a require.
+ * 
+ * @param string name
+ * @return void
+ * 
+ * File extensions, even when just holding html, will be .php.
+ * Thus it is possible to get the file by just passing the name.
+ * Ie. $name = 'footer' instead of $name = 'footer.php'
+ */
+
+function loadPartial($name)
+{
+    require basePath('views/partials/' . $name . '.php');
+}
