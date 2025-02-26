b34aff1f7931d3b35da41c9163faa5cc9d50f4b3
LucasSM3006 <sasso3006@gmail.com>
2025-02-25 22:24:56 -0300

    Added an href that takes you to the editing page

--- php-sandbox/07-database/12-update-record/post.php
+++ php-sandbox/07-database/12-update-record/post.php
@@ -51,10 +51,12 @@ $post = $stmt->fetch();
           <a href="index.php">Go Back</a>
         </div>
       </div>
+      <a href="edit.php?id=<?= $post['id']?>" class="bg-green-500 text-white px-4 py-2 rounded block w-full text-center mb-4 hover:bg-green-600 focus:outline-none">Edit</a>
+
       <form action="delete.php" method="post">
         <input type="hidden" name="_method" value="delete">
         <input type="hidden" name="id" value="<?= $post['id'] ?>">
-        <button type="submit" name="submit" class="text-xl bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 focus:outline-none">Delete</button>
+        <button type="submit" name="submit" class="text-xl bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 focus:outline-none w-full">Delete</button>
       </form>
     </div>
   </div>
