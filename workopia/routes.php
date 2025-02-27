69524feebf76a057b95bc464a13c4262e84df791
LucasSM3006 <sasso3006@gmail.com>
2025-02-26 23:08:49 -0300

    Changed what it does
    No longer returns an array of the available routes, instead adds it to the router object

--- workopia/routes.php
+++ workopia/routes.php
@@ -1,9 +1,5 @@
 <?php
 
-return
-[
-    '/' => 'controllers/home.php', // home
-    '/listings' => 'controllers/listings/index.php', // gets all listings
-    '/listings/create' => 'controllers/listings/create.php', // create
-    '404' => 'controllers/error/404.php' // error page
-];
+$router->get('/', 'controllers/home.php');
+$router->get('/listings', 'controllers/listings/index.php');
+$router->get('/listings/create', 'controllers/listings/create.php');
