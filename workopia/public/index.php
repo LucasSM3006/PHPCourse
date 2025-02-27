411e5adc1418e386d6d5bd1d47e2b92c3f89d3c4
LucasSM3006 <sasso3006@gmail.com>
2025-02-26 21:04:51 -0300

    Router
    Routes array & router. This will take the url and try to find the resource we're looking for. Goes to an error page if not possible.

--- workopia/public/index.php
+++ workopia/public/index.php
@@ -2,5 +2,21 @@
 
 require '../helpers.php';
 
-loadView('home');
-// require './views/home.view.php'; // also works fine
+$routes = 
+[
+    '/' => 'controllers/home.php', // home
+    '/listings' => 'controllers/listings/index.php', // gets all listings
+    '/listings/create' => 'controllers/listings/create.php', // create
+    '404' => 'controllers/error/404.php' // error page
+];
+
+$uri = $_SERVER['REQUEST_URI'];
+
+if(array_key_exists($uri, $routes))
+{
+    require basePath($routes[$uri]);
+}
+else
+{
+    require basePath($routes['404']);
+}
