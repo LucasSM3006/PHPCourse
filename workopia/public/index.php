9aefde07886eec39aa903d6d201391a2816f85ee
LucasSM3006 <sasso3006@gmail.com>
2025-02-26 21:38:05 -0300

    Moved the router logic
    router.php holds the if & else, routes.php holds the available routes. Router will be refactored soon.

--- workopia/public/index.php
+++ workopia/public/index.php
@@ -2,21 +2,6 @@
 
 require '../helpers.php';
 
-$routes = 
-[
-    '/' => 'controllers/home.php', // home
-    '/listings' => 'controllers/listings/index.php', // gets all listings
-    '/listings/create' => 'controllers/listings/create.php', // create
-    '404' => 'controllers/error/404.php' // error page
-];
-
 $uri = $_SERVER['REQUEST_URI'];
 
-if(array_key_exists($uri, $routes))
-{
-    require basePath($routes[$uri]);
-}
-else
-{
-    require basePath($routes['404']);
-}
+require basePath('router.php');
---
+++ workopia/routes.php
@@ -0,0 +1,9 @@
+<?php
+
+return
+[
+    '/' => 'controllers/home.php', // home
+    '/listings' => 'controllers/listings/index.php', // gets all listings
+    '/listings/create' => 'controllers/listings/create.php', // create
+    '404' => 'controllers/error/404.php' // error page
+];
