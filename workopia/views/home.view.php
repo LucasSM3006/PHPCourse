c4a810ad244f3ff3175946397d746d8b70a125a5
LucasSM3006 <sasso3006@gmail.com>
2025-02-27 23:06:51 -0300

    Small bug, had an extra '/' before id.

--- workopia/views/home.view.php
+++ workopia/views/home.view.php
@@ -20,7 +20,7 @@ loadPartial('topbanner');
                 <?= $listing->description ?>
               </p>
               <ul class="my-4 bg-gray-100 p-4 rounded">
-                <li class="mb-2"><strong>Salary:</strong>$<?= formatSalary($listing->salary) ?></li>
+                <li class="mb-2"><strong>Salary:</strong><?= formatSalary($listing->salary) ?></li>
                 <li class="mb-2">
                   <strong>Location:</strong> <?= $listing->city ?>, <?= $listing->state ?>
                   <!-- <span
@@ -32,7 +32,7 @@ loadPartial('topbanner');
                   <strong>Tags:</strong> <span><?= $listing->tags ?></span>
                 </li>
               </ul>
-              <a href="/listing/<?= $listing->id ?>"
+              <a href="/listing?id=<?= $listing->id ?>"
                 class="block w-full text-center px-5 py-2.5 shadow-sm rounded border text-base font-medium text-indigo-700 bg-indigo-100 hover:bg-indigo-200"
               >
                 Details
