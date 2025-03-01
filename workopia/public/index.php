5f49ef7336ca3f2b4c34c5a3d21348f87c24c4db
LucasSM3006 <sasso3006@gmail.com>
2025-02-28 22:32:27 -0300

    Commented out due to composer autoload

--- workopia/public/index.php
+++ workopia/public/index.php
@@ -1,5 +1,5 @@
 <?php
-
+require __DIR__ . '/../vendor/autoload.php';
 require '../helpers.php';
 // require basePath('Framework/Router.php');
 // require basePath('Framework/Database.php');
@@ -7,13 +7,13 @@ require '../helpers.php';
 /* Code below does the same as the two requires above. */
 
 // Will autoload whatever is in Framework/.
-spl_autoload_register(function ($class) {
-    $path = basePath('Framework/' . $class . '.php');
-    if(file_exists($path))
-    {
-        require $path;
-    }
-});
+// spl_autoload_register(function ($class) {
+//     $path = basePath('Framework/' . $class . '.php');
+//     if(file_exists($path))
+//     {
+//         require $path;
+//     }
+// });
 
 $router = new Router();
 
