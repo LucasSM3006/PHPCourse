3d42a9abf874d1366bf9ce051f97cdaf07e7f9d7
LucasSM3006 <sasso3006@gmail.com>
2025-02-28 22:19:59 -0300

    Autoloader instead of require. Will be replaced by composer & PSR-4.

--- workopia/public/index.php
+++ workopia/public/index.php
@@ -1,8 +1,19 @@
 <?php
 
 require '../helpers.php';
-require basePath('Framework/Router.php');
-require basePath('Framework/Database.php');
+// require basePath('Framework/Router.php');
+// require basePath('Framework/Database.php');
+
+/* Code below does the same as the two requires above. */
+
+// Will autoload whatever is in Framework/.
+spl_autoload_register(function ($class) {
+    $path = basePath('Framework/' . $class . '.php');
+    if(file_exists($path))
+    {
+        require $path;
+    }
+});
 
 $router = new Router();
 
