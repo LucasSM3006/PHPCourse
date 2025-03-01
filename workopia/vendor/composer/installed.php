35a0937aeae8bbc1a31d2710867673c392edcd61
LucasSM3006 <sasso3006@gmail.com>
2025-02-28 22:33:21 -0300

    Composer related files. Technically you only need composer.json but might as well since it's a few KB.

---
+++ workopia/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'lucassm3006/phpcourse',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '3d42a9abf874d1366bf9ce051f97cdaf07e7f9d7',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'lucassm3006/phpcourse' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '3d42a9abf874d1366bf9ce051f97cdaf07e7f9d7',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
