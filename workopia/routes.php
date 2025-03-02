fb8457bf8d5821305f07a1d07b322d0540a5ad63
LucasSM3006 <sasso3006@gmail.com>
2025-03-02 20:09:32 -0300

    Routes refactor
    routes.php now registers paths differently

--- workopia/routes.php
+++ workopia/routes.php
@@ -1,6 +1,9 @@
 <?php
 
-$router->get('/', 'controllers/home.php');
-$router->get('/listings', 'controllers/listings/index.php');
-$router->get('/listings/create', 'controllers/listings/create.php');
-$router->get('/listing', 'controllers/listings/show.php');
+// $router->get('/', 'controllers/home.php');
+// $router->get('/listings', 'controllers/listings/index.php');
+// $router->get('/listings/create', 'controllers/listings/create.php');
+// $router->get('/listing', 'controllers/listings/show.php');
+
+// The idea is that we pass the place and then the function.
+$router->get('/', 'HomeController@index');
