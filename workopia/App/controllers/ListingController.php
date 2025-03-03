fabfc749bfd094be8641fdb76b3c3f79b5db58b7
LucasSM3006 <sasso3006@gmail.com>
2025-03-02 23:20:29 -0300

    Documentation comments

--- workopia/App/controllers/ListingController.php
+++ workopia/App/controllers/ListingController.php
@@ -8,10 +8,19 @@ class ListingController
 {
     protected $db;
 
+    /**
+     * Expects no parameters.
+     */
+
     public function __construct() {
         $config = require basePath("config/db.php");
         $this->db = new Database($config);
     }
+
+    /**
+     * Index. Main page.
+     * @return void
+     */
 
     public function index()
     {
@@ -22,10 +31,20 @@ class ListingController
         ]); 
     }
 
+    /**
+     * Create. Loads the create view. Form for submitting job applications.
+     * @return void
+     */
+
     public function create()
     {
         loadView('listings/create');
     }
+
+    /**
+     * Listing. Shows a singular listing based on the ID.
+     * @return void
+     */
 
     public function listing()
     {
