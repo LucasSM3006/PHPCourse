461fde73c6f068f7fcc4c52e2c4722cafed53f71
LucasSM3006 <sasso3006@gmail.com>
2025-03-03 21:25:12 -0300

    First post method

--- workopia/routes.php
+++ workopia/routes.php
@@ -10,3 +10,5 @@ $router->get('/', 'HomeController@index');
 $router->get('/listings', 'ListingController@index');
 $router->get('/listings/create', 'ListingController@create');
 $router->get('/listing/{id}', 'ListingController@listing');
+
+$router->post('/listings', 'ListingController@store');
