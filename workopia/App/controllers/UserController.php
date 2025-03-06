49f9378049037ae6dee135eabad7c11931043164
LucasSM3006 <sasso3006@gmail.com>
2025-03-05 23:31:39 -0300

    Controller for the user, required methods for loading views, and new routes added to routes.php

---
+++ workopia/App/controllers/UserController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Controllers;
+
+use Framework\Database;
+use Framework\Validation;
+
+class UserController
+{
+    protected $db;
+
+    public function __construct() {
+        $config = require basePath('config/db.php');
+        $this->db = new Database($config);
+    }
+
+    /**
+     * Show the login view
+     * 
+     * @return void
+     */
+
+    public function login()
+    {
+        loadview('users/login');
+    }
+
+    /**
+     * Show the register page
+     * 
+     * @return void
+     */
+
+    public function create()
+    {
+        loadview('users/create');
+    }
+
+    /**
+     * Store user in DB
+     * 
+     * @return void
+     */
+
+    public function store()
+    {
+        $MIN_PASS_LENGTH = 6;
+        $MAX_PASS_LENGTH = 50;
+
+        $MIN_NAME_LENGTH = 2;
+        $MAX_NAME_LENGTH = 50;
+
+        $userData =
+        [
+            'name' => $_POST['name'],
+            'email' => $_POST['email'],
+            'city' => $_POST['city'],
+            'state' => $_POST['state'],
+            'password' => $_POST['password'],
+            'password_confirmation' => $_POST['password_confirmation']
+        ];
+
+        $errors = [];
+
+        // Validation
+
+        if(!Validation::email($userData['email']))
+        {
+            $errors['email'] = "Please enter a valid e-mail address";
+        }
+
+        if(!Validation::string($userData['name'], $MIN_NAME_LENGTH, $MAX_NAME_LENGTH))
+        {
+            $errors['name'] = "Name must be between {$MIN_NAME_LENGTH} and {$MAX_NAME_LENGTH} characters";
+        }
+
+        if(!Validation::string($userData['password'], $MIN_PASS_LENGTH, $MAX_PASS_LENGTH))
+        {
+            $errors['password'] = "Password must be between {$MIN_PASS_LENGTH} and {$MAX_PASS_LENGTH} characters";
+        }
+
+        if(!Validation::match($userData['password'], $userData['password_confirmation']))
+        {
+            $errors['password_confirmation'] = "Passwords do not match";
+        }
+
+        if(!empty($errors))
+        {
+            // Reload the view w/ errors!
+            loadView("users/create", [
+                'errors' => $errors,
+                'user' => $userData
+            ]);
+            exit;
+        }
+
+        $params = 
+        [
+            'email' => $userData['email']
+        ];
+        
+        $query = "SELECT * FROM users WHERE email = :email";
+
+        $listing = $this->db->query($query, $params);
+
+        if($listing)
+        {
+            $errors['user_already_exists'] = "Email {$userData['email']} already in use";
+            loadView('users/create', ['errors' => $errors]);
+            exit;
+        }
+    }
+}
--- workopia/routes.php
+++ workopia/routes.php
@@ -17,3 +17,10 @@ $router->post('/listings', 'ListingController@store');
 $router->put('/listings/{id}', 'ListingController@update');
 
 $router->delete('/listings/{id}', 'ListingController@destroy');
+
+// User controller
+
+$router->get('/auth/register', 'UserController@create');
+$router->get('/auth/login', 'UserController@login');
+
+$router->post('/auth/register', 'UserController@store');
