bb187098715918a00fb390ce5d20cb3145b996da
LucasSM3006 <sasso3006@gmail.com>
2025-03-06 20:39:24 -0300

    Work on store(), and work on logout()
    store() sets up session variables & redirects the user now. logout() clears the superglobal & deletes the cookie

--- workopia/App/controllers/UserController.php
+++ workopia/App/controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Controllers;
 
 use Framework\Database;
 use Framework\Validation;
+use Framework\Session;
 
 class UserController
 {
@@ -123,6 +124,31 @@ class UserController
         $query = "INSERT INTO users (name, email, city, state, password) VALUES (:name, :email, :city, :state, :password)";
 
         $this->db->query($query, $userQueryParams);
+
+        // Get new user ID
+        $userId = $this->db->conn->lastInsertId();
+
+        Session::set('user', [
+            'id' => $userId,
+            'name' => $userData['name'],
+            'email' => $userData['email'],
+            'city' => $userData['city'],
+            'state' => $userData['state']
+        ]);
+
+        redirect('/');
+    }
+
+    /**
+     * Logout method to log the user out & destroy the session.
+     * 
+     * @return void
+     */
+    public function logout()
+    {
+        Session::clearAll();
+        $params = session_get_cookie_params();
+        setcookie('PHPSESSID', '', (time() - 86400), $params['path'], $params['domain']);
 
         redirect('/');
     }
