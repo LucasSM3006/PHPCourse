3abcb51717d14787ab5b2c92b6921c0f43aa9018
LucasSM3006 <sasso3006@gmail.com>
2025-03-06 21:55:58 -0300

    Route for authenticate, which is a login method in usercontroller

--- workopia/routes.php
+++ workopia/routes.php
@@ -25,3 +25,4 @@ $router->get('/auth/login', 'UserController@login');
 
 $router->post('/auth/register', 'UserController@store');
 $router->post('/auth/logout', 'UserController@logout');
+$router->post('/auth/login', 'UserController@authenticate');
